60c4e09e23b6a4e43716511566458fb3e5a42c58
mathelenoo <helenomath@gmail.com>
2024-06-21 20:31:07 -0300

    feat: home page com cards e imagens da api de pokedex + barra com filtro

--- src/Components/TextInput/index.tsx
+++ src/Components/TextInput/index.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import  {TextInput}  from 'react-native';
-import {styles} from './style';
+import { TextInput } from "react-native";
+import { styles } from "./style";
 type PropsComponent = {
-    recebeplaceholder: string;
-    recebevalue: string;
-    recebefuncao: (value: string) => void;
-    recebetipoinput: boolean;
+  recebeplaceholder: string;
+  recebevalue: string;
+  recebefuncao: (value: string) => void;
+  recebetipoinput: boolean;
+  onChangeText: (value: string) => void;
+  secureTextEntry?: boolean;
+};
 
-}
-
-export function TextInputComponent({recebefuncao, recebeplaceholder, recebetipoinput, recebevalue}: PropsComponent){
-
-    return(
-        
-        <TextInput style={styles.styleInput} onChangeText={recebefuncao} value={recebevalue} placeholder={recebeplaceholder} secureTextEntry={recebetipoinput}/>
-    )
+export function TextInputComponent({
+  recebefuncao,
+  recebeplaceholder,
+  recebetipoinput,
+  recebevalue,
+}: PropsComponent) {
+  return (
+    <TextInput
+      style={styles.styleInput}
+      onChangeText={recebefuncao}
+      value={recebevalue}
+      placeholder={recebeplaceholder}
+      secureTextEntry={recebetipoinput}
+    />
+  );
 }

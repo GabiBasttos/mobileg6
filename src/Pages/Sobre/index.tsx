dd72f758be5d9e99f3f7c7d7aee490a07c2a5c92
iza.pdelucca <bellapdelucca@gmail.com>
2024-06-24 15:06:59 -0300

    feat: adicionando  novas funções p/ sobre e estilização

--- src/Pages/Sobre/index.tsx
+++ src/Pages/Sobre/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import { useRoute, RouteProp } from "@react-navigation/native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import { useRoute, RouteProp, useNavigation } from "@react-navigation/native";
 import axios from "axios";
 import { styles } from "./style";
 
@@ -10,17 +10,73 @@ type RouteParams = {
   };
 };
 
+const tipoCor: { [key: string]: string } = {
+  bug: "#8BD674",
+  dark: "#6F6E78",
+  dragon: "#7383B9",
+  electric: "#F2CB55",
+  fairy: "#EBA8C3",
+  fighting: "#EB4971",
+  fire: "#FFA756",
+  flying: "#83A2E3",
+  ghost: "#8571BE",
+  grass: "#8BBE8A",
+  ground: "#F78551",
+  ice: "#91D8DF",
+  normal: "#B5B9C4",
+  poison: "#9F6E97",
+  psychic: "#FF6568",
+  rock: "#D4C294",
+  steel: "#4C91B2",
+  water: "#58ABF6",
+};
+
 export function SobrePokemon() {
   const route = useRoute<RouteProp<RouteParams, "SobrePokemon">>();
   const { pokemonUrl } = route.params;
+  const navigation = useNavigation();
 
   const [pokemonData, setPokemonData] = useState<any>(null);
+  const [descricao, setDescricao] = useState<string | null>(null);
+  const [evolucao, setEvolucao] = useState<any[]>([]);
+  const [favorito, setFavorito] = useState(false);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
         const response = await axios.get(pokemonUrl);
         setPokemonData(response.data);
+
+        const speciesResponse = await axios.get(response.data.species.url);
+        const evolutionChainResponse = await axios.get(
+          speciesResponse.data.evolution_chain.url
+        );
+
+        const descriptionEntry = speciesResponse.data.flavor_text_entries.find(
+          (entry: any) => entry.language.name === "en"
+        );
+        setDescricao(
+          descriptionEntry
+            ? descriptionEntry.flavor_text
+            : "No description available."
+        );
+
+        const evolutionData = evolutionChainResponse.data.chain;
+        const evolutionArray = [];
+        let currentEvolution = evolutionData;
+
+        while (currentEvolution) {
+          const evolutionDetails = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${currentEvolution.species.name}`
+          );
+          evolutionArray.push({
+            name: currentEvolution.species.name,
+            image: evolutionDetails.data.sprites.front_default,
+          });
+          currentEvolution = currentEvolution.evolves_to[0];
+        }
+
+        setEvolucao(evolutionArray);
       } catch (error) {
         console.error("Erro ao buscar informações do Pokémon:", error);
       }
@@ -28,6 +84,10 @@ export function SobrePokemon() {
 
     fetchPokemonData();
   }, [pokemonUrl]);
+
+  const tornarFavorito = () => {
+    setFavorito(!favorito);
+  };
 
   if (!pokemonData) {
     return (
@@ -37,14 +97,35 @@ export function SobrePokemon() {
     );
   }
 
+    const backgroundColor = pokemonData.types.length
+      ? tipoCor[pokemonData.types[0].type.name]
+      : "#FFF";
+
+    const color = pokemonData.types.length
+      ? tipoCor[pokemonData.types[0].type.name]
+      : "#FFF";
+
+
   return (
-    <View>
+    <ScrollView style={{ backgroundColor }}>
       <View style={styles.container_top}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
           <Image
             source={require("../../Assets/setaVoltar.png")}
             alt="seta de voltar"
             style={styles.seta}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={tornarFavorito}>
+          <Image
+            source={
+              favorito
+                ? require("../../Assets/iconeFavoritoClicado.png")
+                : require("../../Assets/iconeFavorito.png")
+            }
+            alt="ícone de coração para favoritar pokemon"
+            style={styles.heart}
           />
         </TouchableOpacity>
 
@@ -54,20 +135,55 @@ export function SobrePokemon() {
           style={styles.pokemon}
         />
 
-        <Text style={styles.name}>
+        <Text style={styles.nome}>
           {pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1)}
         </Text>
+
         <Text style={styles.number}>
           #{pokemonData.id.toString().padStart(3, "0")}
         </Text>
-        <Text style={styles.about}>Sobre o Pokémon</Text>
+
+        {pokemonData.types.map((typeInfo: any) => (
+          <Text key={typeInfo.type.name} style={styles.tipo}>
+            {typeInfo.type.name}
+          </Text>
+        ))}
+
+        <Text style={styles.sobrePokemon}>Sobre o Pokémon:</Text>
       </View>
 
       <View style={styles.container_bottom}>
-        <Text>Descrição</Text>
-        <Text>{pokemonData.description}</Text>
-        <Text>Evoluções</Text>
+        <Text style={[styles.topicos, { color }]}>Descrição:</Text>
+        <Text style={styles.text}>{descricao}</Text>
+
+        <Text style={[styles.topicos, { color }]}>Habilidades:</Text>
+        {pokemonData.abilities.map((abilityInfo: any) => (
+          <Text key={abilityInfo.ability.name} style={styles.text}>
+            {abilityInfo.ability.name}
+          </Text>
+        ))}
+
+        <Text style={[styles.topicos, { color }]}>Evoluções:</Text>
+        {evolucao.map((evolucao) => (
+          <View key={evolucao.name} style={styles.evolucao_container}>
+            <Image
+              source={{ uri: evolucao.image }}
+              style={styles.evolucao_img}
+            />
+            <Image
+              source={require("../../Assets/iconeSetaBaixo.png")}
+              alt="seta para baixo"
+              style={{
+                tintColor: "#000",
+                top: 120,
+                right: 50,
+                width: 65,
+                height: 65,
+              }}
+            />
+          </View>
+        ))}
       </View>
-    </View>
+    </ScrollView>
   );
 }
--- src/Pages/Sobre/style.ts
+++ src/Pages/Sobre/style.ts
@@ -2,30 +2,36 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container_top: {
-    backgroundColor: "#EA5D60",
-    height: 354,
+    height: 330,
   },
 
   container_bottom: {
     borderRadius: 10,
     backgroundColor: "#F6F6F6",
+    height: 950,
   },
 
   seta: {
     width: 36,
     height: 36,
-    top: 25,
+    top: 40,
     left: 14,
   },
 
-  heart: {},
+  heart: {
+    position: "absolute",
+    width: 36,
+    height: 36,
+    top: 5,
+    right: 14,
+  },
 
-  name: {
+  nome: {
     fontFamily: "Inter_700Bold",
     fontSize: 32,
     textAlign: "left",
     left: 200,
-    bottom: 70,
+    bottom: 80,
     color: "#fff",
   },
 
@@ -34,24 +40,65 @@ export const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: "left",
     left: 200,
-    bottom: 75,
+    bottom: 85,
     color: "#747476",
   },
 
   pokemon: {
     width: 180,
     height: 180,
-    top: 60,
+    top: 40,
     left: 25,
   },
 
-  about: {
+  tipo: {
+    fontFamily: "Inter_700Bold",
+    fontSize: 15,
+    textAlign: "left",
+    left: 200,
+    bottom: 90,
+  },
+
+  sobrePokemon: {
     fontFamily: "Inter_700Bold",
     fontSize: 20,
     textAlign: "left",
     left: 111,
-    top: 30,
-    bottom: 50,
+    bottom: 35,
     color: "#fff",
+  },
+
+  topicos: {
+    fontFamily: "Inter_700Bold",
+    fontSize: 18,
+    textAlign: "left",
+    left: 30,
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    top: 10,
+  },
+
+  text: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 15,
+    left: 30,
+    top: 3,
+    bottom: 60,
+    marginLeft: 10,
+    color: "#000",
+  },
+
+  evolucao_container: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+    bottom: 30,
+  },
+
+  evolucao_img: {
+    width: 220,
+    height: 220,
+    left: 100,
   },
 });

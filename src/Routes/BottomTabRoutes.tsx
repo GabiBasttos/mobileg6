6b61e74b5ed1b0e532e8ef79ae0283be8ac9d8a4
Emmilly <mimig2250@gmail.com>
2024-06-24 16:52:56 -0300

    feat: definindo o botão de logout na bottomtab.

--- src/Routes/BottomTabRoutes.tsx
+++ src/Routes/BottomTabRoutes.tsx
@@ -1,17 +1,26 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { Home } from "../Pages/Home";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View, Alert } from "react-native";
 import iconehome from "../Assets/iconeHome.png";
 import { Perfil } from '../Pages/Perfil';
 import Person from '../Assets/iconePerfil.png';
 import { Login } from "../Pages/Login";
 import Sair from '../Assets/iconeSair.png';
 import { styles } from "./style";
+import { useNavigation } from "@react-navigation/native";
 
 const TabAll = createBottomTabNavigator();
 
 export function BottomTabRoutes() {
+    const navigation = useNavigation();
+
+    const navegando = useNavigation();
+
+    const handleLogout = () =>{
+    navegando.navigate("StackLogin", { name: "Login" })
+  }
+
     return (
         <TabAll.Navigator
             screenOptions={{
@@ -48,7 +57,8 @@ export function BottomTabRoutes() {
                             <Image source={Sair} style={{ tintColor: focused ? '#EA5D60' : '#000', width: 32, height: 32 }} />
                             <Text style={{ fontSize: 14, color: focused ? '#EA5D60' : '#000', fontFamily: 'Inter_400Regular' }}>Sair</Text>
                         </View>
-                    ),
+                    ),   tabBarButton: (props) => (
+                        <TouchableOpacity {...props} onPress={handleLogout} /> ),
                 }} />
         </TabAll.Navigator>
     )
